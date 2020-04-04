0ae0afcf86e387af59169e0d050186e754afcea1
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-04 16:41:06 -0700

    Create SampleTemplateFunction.cpp

---
+++ SampleTemplateFunction.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+template <typename T, typename U>
+T getBigger(T input1, U input2);
+
+
+int main()
+{
+    int a = 5;
+    float b = 6.334;
+    int bigger;
+    cout<<"Between "<<a<<" and "<<b<<" "<<getBigger(a,b)<<" is bigger.\n";
+
+    cout<<"Between "<<a<<" and "<<b<<" "<<getBigger(b,a)<<" is bigger.\n";    
+    return 0;
+}
+
+template <typename T, typename U>
+T getBigger(T input1, U input2)
+{
+    if(input1 > input2)
+        return input1;
+    return input2;
+}
