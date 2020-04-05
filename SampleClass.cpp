25e37e420536f7d2fcf28f3ea65a59f77ead9802
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-04 21:32:06 -0700

    Create SampleClass.cpp

---
+++ SampleClass.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    Dog dog1, dog2;
+    dog1.setName("Trixie");
+    dog2.setName("Kali");
+    dog1.setLicenseNumber(1234);
+    dog2.setLicenseNumber(5678);
+
+    dog1.printInfo();
+    cout<<"\n";
+    dog2.printInfo();
+    return 0;
+}
+
+
+class Dog
+{
+    string name;
+    int licenseNumber;
+public:
+    void setName(string nameIn);
+    void setLicenseNumber(int licenseNumberIn);
+    string getName();
+    int getLicenseNumber();
+    void printInfo();
+};
+
+void Dog::setName(string nameIn)
+{
+    name = nameIn;
+}
+
+void Dog::setLicenseNumber(int licenseNumberIn)
+{
+    licenseNumber = licenseNumberIn;
+}
+
+string Dog::getName()
+{
+    return name;
+}
+
+int Dog::getLicenseNumber()
+{
+    return licenseNumber;
+}
+
+void Dog::printInfo()
+{
+    cout<<name<<" "<<licenseNumber;
+}
