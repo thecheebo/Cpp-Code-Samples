757cca15c6a031e2fc7c50790a00df5ad5e280a5
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-09 18:49:19 -0700

    Create GraphADT.cpp

---
+++ GraphADT.cpp
@@ -0,0 +1,12 @@
+Functions:
+insertVertex(K Key);
+insertEdge(Vertex v1, Vertex v2, K Key);  //maybe data
+
+removeVertex(Vertex v);
+removeEdge(Vertex v1, Vertex v2);
+
+incidentEdges(Vertex v);  //what are the incident edges
+areAdjacent(Vertex v1, Vertex v2);  //see if two vertices  are adjacent
+
+origin(Edge e);   //IF graph is directional
+destination(Edge e); //IF graph is directional
