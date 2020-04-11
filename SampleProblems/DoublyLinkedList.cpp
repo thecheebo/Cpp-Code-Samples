2195452c82766a766f26976f913aeb4e5f32f0e5
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-11 11:03:54 -0700

    Create DoublyLinkedList.cpp

---
+++ SampleProblems/DoublyLinkedList.cpp
@@ -0,0 +1,31 @@
+class ListNode {
+public:
+//Attributes
+  int element;
+  ListNode* next;
+  ListNode* prev;
+
+
+//Functions
+  void RemoveSublist(ListNode* headtarget, int int_in_the_list, int width_removal); //headtarget points to first node
+  
+  ListNode* headtarget1 = headtarget;
+  
+  while(headtarget1.element != value);
+    headtarget1 = headtarget1->next;
+    
+  Listnode * headtargetcopy = headtarget1;
+  
+  for (int i=0; i <= width_removal; i++) {
+    headtarget1 = headtarget1->next;
+  }  
+  
+  for (int i=0; i <= width_removal; i++) {
+    headtargetcopy = headtargetcopy->prev;
+    
+  headtargetcopy.next = headtarget1
+  
+    
+    
+};
+
