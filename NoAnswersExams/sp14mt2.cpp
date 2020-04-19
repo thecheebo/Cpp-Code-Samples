9037e8844f1b67edaa192c0a7d12fb3cb4a7bac5
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-19 16:20:12 -0700

    Create sp14mt2.cpp

---
+++ NoAnswersExams/sp14mt2.cpp
@@ -0,0 +1,12 @@
+1. [Miscellaneous – 35 points]
+
+MC1 
+
+Queue, singly linked list, head and tail pointers, front of the queue is the tail, and rear is the head
+Worst running time for enqueu and dequeue
+
+Answer: O(1) for both functions
+
+MC2
+
+C.5 , 2?
