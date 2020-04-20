a52db5c9ac49de4703a98c0aa0c11a674c449726
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-20 13:05:50 -0700

    Update sp14mt2.cpp

--- NoAnswersExams/Midterm2/sp14mt2.cpp
+++ NoAnswersExams/Midterm2/sp14mt2.cpp
@@ -10,3 +10,70 @@ Answer: O(1) for both functions
 MC2
 
 C.5 , 2?
+
+MC3  
+
+c. 
+
+MC4
+
+b.
+  
+MC5
+
+b. 
+  
+1
+2
+4
+8
+2
+  
+MC6
+
+D
+S. but maybe c.
+  
+MC7
+
+BST AND AVL? a) 1.
+  trick quesiton bst can end up being a lInked list.
+  
+MC8.
+ 
+  d.  912 greater the 911
+
+MC9.
+  C
+  
+MC10.
+  look up B tree, how many disk seeks
+  
+MC11
+e?
+MC12, havnt learned huffman tree.
+  
+MC13
+
+what the fk is it asking
+
+MC14
+
+e?
+  
+15. o(1) a
+16. o(nlogn) d
+17. O(1)
+18. O(1)
+19. O(1)
+20. O(1)
+21. O(n)
+22. O(nlogn)
+  
+MP4ish
+
+a same?
+b? queue
+c.
+  
+  
