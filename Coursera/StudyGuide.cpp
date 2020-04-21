c6594e9cb12bbf7defc1617568ca81f8337dca89
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-20 18:16:39 -0700

    Create StudyGuide.cpp

---
+++ Coursera/StudyGuide.cpp
@@ -0,0 +1,72 @@
+***** Part 3 - Unordered Data Structures *****
+
+
+
+***** Week 1 *****
+
+Hashing
+Hash Functions
+Chaining
+Probing/doubleHashing
+Analysis
+Hash Tables in C++
+
+***** Week 2 *****
+dISJOINT seTS
+Union Find
+Naive Implementation
+
+Disjoin-UpTrees Implementation
+
+Run times
+Path COmpression I
+II
+
+
+
+***** Week 3 *****
+
+Graphs
+
+    Incident Edges - 
+    Degree - The count of incident edge
+
+    Adjacenct Edges - 
+
+Edge List
+
+Adjacency Matrix
+
+Adjacency List
+    
+    
+
+***** Week 4 *****
+
+BFS
+
+DFS
+
+MST
+
+Kruskal's Algorithm
+
+Prim's Algorithm
+
+Dijkstra's Algorithm
+
+Landmark Path Problem
+
+
+
+Complexity to find shortest path, from 1 vertex to all others in directed weighted graph
+O(m + n lg n)
+
+
+For which one case below will Dijkstra's algorithm fail to produce a shortest path?
+
+A connected graph with a cycle of negative weight edges.
+
+Correct
+Dijkstra's algorithm would continue to cycle through these negative edges, accumulating
+a shorter path in terms of length as measured by edge weight path sum and would never terminate.
