1fdeed9a1ff81868a1a6cf13d0e5fe8149a4406d
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-29 19:50:48 -0700

    Update DijkstrasAlgorithm.h

--- Coursera/DijkstrasAlgorithm.h
+++ Coursera/DijkstrasAlgorithm.h
@@ -14,3 +14,25 @@ table implementation of the operation. With enough edges, this cost makes the ov
 cost more expensive than the table implementation, despite the faster search time. For
 sparse graphs, we don’t have quite so many edges, so the improved search time outweighs
 the worsened update costs.
+
+
+ (e) For the given graph, run Dijkstra’s algorithm, indicating in the table below the distances
+at each vertex at the end of each step (dv), and whether or not the vertex has been marked
+known yet at the end of each step (kv). The initialization has already been done for you.
+A B C D E F G
+A 0 8 3 0 0 0 0
+B 0 0 0 0 0 16 18
+C 0 3 0 0 0 0 20
+D 0 0 0 0 0 0 19
+E 5 0 12 2 0 0 0
+F 0 0 0 0 0 0 0
+G 0 0 0 0 0 10 0
+V dv kv dv kv dv kv dv kv dv kv dv kv dv kv dv kv
+A ∞ 0 5 0 5 0 5 1 5 1 5 1 5 1 5 1
+B ∞ 0 ∞ 0 ∞ 0 13 0 11 0 11 1 11 1 11 1
+C ∞ 0 12 0 12 0 8 0 8 1 8 1 8 1 8 1
+D ∞ 0 2 0 2 1 2 1 2 1 2 1 2 1 2 1
+E 0 0 0 1 0 1 0 1 0 1 0 1 0 1 0 1
+F ∞ 0 ∞ 0 ∞ 0 ∞ 0 ∞ 0 27 0 27 0 27 1
+G ∞ 0 ∞ 0 21 0 21 0 21 0 21 0 21 1 21 1
+- Start Step 1 Step 2 Step 3 Step 4 Step 5 Step 6 Step 7
