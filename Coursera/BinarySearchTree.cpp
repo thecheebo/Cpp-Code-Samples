e43fa7d0d24c93c3ee46461dc504454c12334934
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-29 21:38:01 -0700

    Create BinarySearchTree.cpp

---
+++ Coursera/BinarySearchTree.cpp
@@ -0,0 +1,9 @@
+MC3 (2.5pts)
+Suppose that we have numbers between 1 and 1000 in a binary search tree and we want to
+search for the number 363. Which of the following sequences can not be the sequence of nodes
+visited in the search?
+(a) 2, 252, 401, 398, 330, 344, 397, 363
+(b) 924, 220, 911, 244, 898, 258, 362, 363
+(c) 2, 399, 387, 219, 266, 382, 381, 278, 363
+(d) 925, 202, 911, 240, 912, 245, 363 < ---
+(e) 935, 278, 347, 621, 399, 392, 358, 363
