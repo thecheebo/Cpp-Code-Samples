933b27ea8c4f7f5c35823a0ae5ca4c6cec003993
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-30 12:18:27 -0700

    Update DisjointSet.cpp

--- Coursera/DisjointSet.cpp
+++ Coursera/DisjointSet.cpp
@@ -12,3 +12,16 @@ smaller sized tree to the larger sized tree, you increase the depth of fewer nod
 you had done things the other way around. That is, union-by-size keeps the expected
 depth of a node as small as possible, by always choosing to increase the depth of fewer
 nodes rather than more nodes.
+
+QUESTION!!
+  
+  What was the “problem” with using path compression and union-by-height together?
+That is, what difficulty does using the two techniques together present? Please be
+specific. (The word “problem” is in quotes because we said it turned out that this
+“problem” didn’t actually affect things too badly, even if it seems like it would.)
+  
+Path compression (potentially) changes the height of the tree, due to (potentially) 
+  shortening a path that was (potentially) the deepest path in the tree. In that case, the height
+stored at the root would become incorrect, as neither path compression nor union-byheight
+has any provision for recalculating the correct height in such a circumstance (and
+adding one would make either algorithm far too expensive, time-wise).
