da4f387d75111d14848a99f7cd1de35569afae07
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-29 19:31:01 -0700

    Create BtreeQuestions.h

---
+++ Coursera/BtreeQuestions.h
@@ -0,0 +1,30 @@
+Which one of the following is NOT a valid reason to choose the B-Tree representation over a standard AVL binary search tree?
+
+
+B-Trees have better algorithmic "Big-O" run-time complexity for the find operation. *** FALSE***
+B-Trees require fewer block read accesses for tree operations.
+B-Trees run faster on large data sets than do AVL trees.
+B-Trees work faster in networked cloud environments than do AVL trees.
+
+Correct
+While the B-Tree find operation runs in O(log_m n) time, the m ends up being a constant factor and O(log_m n) = O(lg n)
+as a consequence of the big-O characterization of how run time increases as the number of data items (n) increases.
+
+
+Which of the following statements is NOT true for a B-Tree of order m?
+
+all true:
+Each node can have at most one more child than key.
+Any node that is not the root or a leaf holds at least half of the total number of keys allowed in a node.
+All leaf nodes are at the same level of the B-Tree.
+
+
+
+If a B-Tree is completely filled, meaning every node holds its maximum number of keys and all non-leaf nodes has
+the maximum number of children, then what happens when an additional key is inserted into the B-Tree?
+
+
+After searching for the leaf node where the new key should go, the leaf is split in half as two separate leaf nodes,
+and then the middle value is thrown up to the layer above as an inserted key, and this insertion and rebalancing
+repeats until a new root key rises to the top, which adds a layer to the tree.
+
