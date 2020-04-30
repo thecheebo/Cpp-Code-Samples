ad16ee46bcbda1f10eecce0105d479006c777bf9
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-29 21:36:17 -0700

    Create PrePostInOrderTraversal.h

---
+++ Coursera/PrePostInOrderTraversal.h
@@ -0,0 +1,17 @@
+A post-order traversal of the following tree visits the nodes in which order?
+
+       g
+      / \
+     c   h
+    /\     \
+   b  e      k
+  /   /\     /\ 
+ a    d f    i l
+            /
+           j
+           
+(a) G C B A E D F H K I J L
+(b) A B C D E F G H I J K L
+(c) A B D F E C J I L K H G <---
+(d) All of these are valid post-order traversals.
+(e) None of these is a valid post-order traversal.
