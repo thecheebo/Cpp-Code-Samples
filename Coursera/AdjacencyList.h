98c86fb2b782e3582707593c73c0b4b49bdea586
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-29 19:58:07 -0700

    Update AdjacencyList.h

--- Coursera/AdjacencyList.h
+++ Coursera/AdjacencyList.h
@@ -39,3 +39,40 @@ graph[ptr->index] = temp;
 ptr = ptr->next;
 }
 }
+
+  
+  
+  Ghost of Euler
+  You have the following EdgeNode class:
+class EdgeNode {
+public:
+int index; // index of target vertex
+EdgeNode* next; // ptr to next edge
+};
+Furthermore, you have a variable of type Array<EdgeNode*> that is indexed from 1 to the
+size of the array (given by the Size() method in the Array class). The array is an adjacency
+list implementation of a graph, of the kind we first discussed in lecture; the vertices have
+indices from 1 to Size().
+You want to write a method numOddDegree which has one parameter, a reference to an
+Array<EdgeNode*> as described above. You can assume this adjacency list represents an
+undirected graph. You want to return the number of vertices with odd degree. (The degree
+of a vertex in an undirected graph, is the number of edges which that vertex is an endpoint
+for. So, you want to know how many vertices are endpoint to an odd number of edges.) You
+can assume the graph has no self-loops (i.e. no vertex has an edge to itself.)
+int numOddDegree(Array<EdgeNode*>& graph) {
+// your code goes here
+int numOdd = 0;
+int currentVert = 1;
+while (currentVert <= graph.Size()) {
+EdgeNode* trav = graph[currentVert];
+int numEdges = 0;
+while (trav != NULL) {
+trav = trav->next;
+numEdges++;
+}
+if (numEdges % 2 == 1)
+numOdd++;
+currentVert++;
+}
+return numOdd;
+}
