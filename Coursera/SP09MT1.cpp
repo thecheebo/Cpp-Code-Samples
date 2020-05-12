d5f8477c374d8a6219affc91ea65c932a90579a4
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-05-11 17:19:49 -0700

    Create SP09MT1.cpp

---
+++ Coursera/SP09MT1.cpp
@@ -0,0 +1,55 @@
+MC1) D.
+
+MC2) D.
+
+MC3) C.
+
+MC4) A.
+
+MC5) B and C.
+
+MC6) A and C.
+
+3. a. :: scope resolution operator
+Private: vs public:
+list initializer in classes 
+inheritance
+ternary operator
+
+b. const can be used at the end of a method, and that means that nothing can be changed within it. 
+
+const at the beginning of a method means it returns a const variable
+
+const in the parameter means it can't be changed itself.
+
+c.encapsulation/inheritance/polymorphism
+
+4.a.
+5.a.
+
+template <class LIT>
+List<LIT>::List(): size(0)
+{
+head = new listNode;
+tail = new listNode;
+head->next = tail.
+}
+
+template <class LIT>
+void List<LIT>::insert(int loc, LIT e)
+{
+listNode * t = new listNode(e);
+listNode * p = Find(loc, head);
+
+listNode * temp = p->next;
+p->next = t;
+t->next = temp;
+size++;
+
+e) List<Int> newlist;
+
+if(!head)
+{removeAll(head->next);
+delete head, tail;
+
+}
