574010aed7014140b15789e6a2c05fddd9a48d1f
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-05-13 15:45:32 -0700

    Update fa09final.cpp

--- Coursera/fa09final.cpp
+++ Coursera/fa09final.cpp
@@ -1,1 +1,24 @@
-MC1
+MC1) c
+
+MC2) c
+  
+MC3) a
+
+MC4) d
+
+MC5) a
+  
+MC6) e
+  
+MC7) C
+
+MC8) D
+  
+MC9) unknown
+
+MC10) got to check uptrees
+
+1A)
+    delete bebo[1];
+  delete []bebo;
+  delete []orkut;
